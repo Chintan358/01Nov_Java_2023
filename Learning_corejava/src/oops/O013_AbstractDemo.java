4e01b5cf2cfa68a80f5f193e5ff20415eb462170
clds <chintan.tops@gmail.com>
2023-12-12 12:25:31 +0530

    abstraction

---
+++ Learning_corejava/src/oops/O013_AbstractDemo.java
@@ -0,0 +1,29 @@
+package oops;
+
+abstract class Abs
+{
+	public abstract void display();
+}
+
+class AbsImpl extends Abs
+{
+
+	@Override
+	public void display() {
+		
+		System.out.println("runing display");
+	}
+	
+}
+
+
+public class O013_AbstractDemo {
+	public static void main(String[] args) {
+		
+		//Abs abs  = new Abs();
+		AbsImpl impl = new AbsImpl();
+		impl.display();
+		
+		
+	}
+}
