2375a7dafb14e76db82594684473d7adad24a585
clds <chintan.tops@gmail.com>
2023-12-15 12:26:14 +0530

    exception

---
+++ Learning_corejava/src/exeption/Demo.java
@@ -0,0 +1,7 @@
+package exeption;
+
+public class Demo {
+	public Demo() {
+		System.out.println("Demo const call....");
+	}
+}
---
+++ Learning_corejava/src/impclasses/I005_ObjectDemo.java
@@ -0,0 +1,20 @@
+package impclasses;
+
+class Sample
+{
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "hello";
+	}
+}
+
+public class I005_ObjectDemo {
+	public static void main(String[] args) {
+		
+		
+		Sample s1 = new Sample();
+		System.out.println(s1);
+		
+	}
+}
